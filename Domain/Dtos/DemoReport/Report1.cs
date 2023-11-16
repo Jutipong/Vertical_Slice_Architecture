135c7696ad75a229acbfdad33df33c7deb7f2aa6
T660046@tcrbank.com <T660046@tcrbank.com>
2023-11-16 17:26:06 +0700

    add: Presentation.Api.Report

---
+++ Domain/Dtos/DemoReport/Report1.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Dtos.DemoReport;
+public class Report1
+{
+    public string Title { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+}
--- Presentation.Api.Report/Endpoints/DemoReport.cs
+++ Presentation.Api.Report/Endpoints/DemoReport.cs
@@ -1,5 +1,36 @@
-﻿namespace Presentation.Api.Report.Endpoints;
+﻿using Carter;
+using Wkhtmltopdf.NetCore;
+using Wkhtmltopdf.NetCore.Options;
 
-public class DemoReport
+namespace Presentation.Api.Report.Endpoints;
+
+public class DemoReport : CarterModule
 {
+    public DemoReport() : base("DemoReport")
+    {
+        this.WithTags("DemoReport");
+    }
+
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("", async (IGeneratePdf _generatePdf) =>
+        {
+            _generatePdf.SetConvertOptions(new ConvertOptions
+            {
+                PageOrientation = Orientation.Portrait,
+                PageSize = Size.A4,
+                PageMargins = new Margins()
+                {
+                    Top = 10,
+                    Left = 5,
+                    Right = 5,
+                    Bottom = 10,
+                },
+            });
+
+            var mockData = new Domain.Dtos.DemoReport.Report1 { Title = "Title", Name = "Name" };
+            var byteArray = await _generatePdf.GetByteArray("~/Views/DemoReport/Report1.cshtml", mockData);
+            return Results.File(byteArray, "application/pdf", $"{DateTime.Now}.pdf");
+        });
+    }
 }
